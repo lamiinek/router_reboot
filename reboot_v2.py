39c27361779006b121f9644c90e7d39b97d945b8
Lamine Ka <lamiinek@gmail.fr>
2016-05-05 14:34:52 +0000

    Update reboot_v2.py

--- reboot_v2.py
+++ reboot_v2.py
@@ -4,7 +4,7 @@
 Aut: Lamine KA (@lamiinek)
 
 Des: This version of the script, give the option to let the router reboot till the internet connection is working properly.
-     The default loop interval is 3 minutes
+     The default loop interval is 2 minutes and 15 secs
 """
 
 import sys, time
@@ -16,27 +16,28 @@ import socket
 class Reboot_router:
 
 	def __init__(self):
+
+		self.url = "192.168.1.1/rebootinfo.cgi"
+		self.user = "admin"
+		self.passw = "xxxx"
 		self.con = False
 
 		self.reboot()
 
 
 	def reboot(self):
-		url = "192.168.1.1/rebootinfo.cgi"
-		user = "admin"
-		passw = "xxxxx"
 
 		opts = input("Options: \n\tr = reboot\n\trl = loop reboot till connected\n\tc = cancel\n> ").lower()
 
 		if opts == "r":
 			
 			print("Rebooting")
-			req = requests.get("http://"+user+":"+passw+"@"+url)
+			req = requests.get("http://"+self.user+":"+self.passw+"@"+self.url)
 
 		elif opts == "rl":
 			
 			print("Loop Reboot mode started!\n")
-			delay = 180 # 3 minutes
+			delay = 135 # 2 minutes 15 secs
 			while self.con == False:
 				self.check_internet()
 				time.sleep(delay)
@@ -52,8 +53,11 @@ class Reboot_router:
 			s = socket.create_connection((h, 80), 2)
 			print(time.strftime("%H:%M:%S")+" - connected")
 			self.con = True
+			
 		except:
 			print(time.strftime("%H:%M:%S")+" - No internet access")
+			#reboot
+			requests.get("http://"+self.user+":"+self.passw+"@"+self.url)
 
 
 rr = Reboot_router()
